8382fce81c510c82a38ef6d6c7bb1dfc5d03ae32
Bono Cichowski <2206279cichowski@zuyd.nl>
2024-12-06 15:34:25 +0100

    Model gelijk aan class diagram

--- TruthOrDrink/Models/Game.cs
+++ TruthOrDrink/Models/Game.cs
@@ -2,9 +2,10 @@
 {
     public class Game
     {
-        public int Gewaagdheid { get; set; }
-        public string Categorie { get; set; }
-        public string Vragenlijst { get; set; }
-        public int Rondes { get; set; }
+        public int Id { get; set; }
+        public int Rounds { get; set; }
+        public string Difficulty { get; set; }
+        public List<Player> Players { get; set; }
+        public List<Question> Questions { get; set; }
     }
 }
