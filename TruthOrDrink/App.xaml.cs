ebb9742bed3c060906a1699fae3e552e5e593916
Bono Cichowski <2206279cichowski@zuyd.nl>
2025-01-05 16:15:18 +0100

    create question into DB

--- TruthOrDrink/App.xaml.cs
+++ TruthOrDrink/App.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.Maui.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using TruthOrDrink;
+
+
 
 namespace TruthOrDrink
 {
     public partial class App : Application
     {
-        public App()
+        public static IServiceProvider Services { get; private set; }
+        public App(IServiceProvider services)
         {
             InitializeComponent();
+            Services = services;
+
 
             //MainPage = new NavigationPage(new MainPage());  //haal deze comment weg als je login scherm wilt zien
             MainPage = new NavigationPage(new StartPage());
--- TruthOrDrink/Models/Question.cs
+++ TruthOrDrink/Models/Question.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,24 @@ using System.Threading.Tasks;
 
 namespace TruthOrDrink.Models
 {
+    [Table("Questions")]
     public class Question
     {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("Id")]
         public int Id { get; set; }
+
+        [Column("Difficulty")]
         public string Difficulty { get; set; }
+
+        [Column("Category")]
         public string Category { get; set; }
+
+        [Column("DefaultQuestion")]
         public string DefaultQuestion { get; set; }
+
+        [Column("PlayerQuestion")]
         public string PlayerQuestion { get; set; }
     }
 }
